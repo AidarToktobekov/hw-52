cebd95b87823ef23f0b3e5fee45760bedb87c0fa
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-05-21 12:28:26 +0600

    1-й уровень готов, на страницу выводится карта, по методам: "rank" и "suit"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Card from './components/card/card.tsx'
 
 const App =()=> {
 
   return (
     <>
-     
+    <div className="playingCards faceImages">
+      <Card rank={'a'} suit={'spades'}/>
+    </div>
     </>
   )
 }
